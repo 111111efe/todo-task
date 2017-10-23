00ce9614f2c740b9df9f77a918200c2b62114a1c
Boanerget <antonyprogramacion96@gmail.com>
2017-10-23 12:24:27 -0400

    commit initial

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{ 
+    'name': 'To-Do Application', 
+    'description': 'Manage your personal To-Do tasks.', 
+    'author': 'Daniel Reis', 
+    'depends': ['base'], 
+    'application': True,
+
+    'data': [
+    	'views/todo_menu.xml',
+    	'views/todo_view.xml'
+    ],
+}
